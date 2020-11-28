8b195200f16d63585d4edb8cb0904aeb8eebc2cf
ArtemVetik <av.unitygames@gmail.com>
2020-11-28 11:30:25 +0300

    Change layer collision matrix

--- Assets/Scripts/Camera/CameraFolowing.cs
+++ Assets/Scripts/Camera/CameraFolowing.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraFolowing : MonoBehaviour
 {
-    [SerializeField] private Transform _player;
+    [SerializeField] private Rigidbody _player;
     [SerializeField] private float _speed;
     [SerializeField] private float _angle;
     [SerializeField] private float _distance;
@@ -20,7 +20,7 @@ public class CameraFolowing : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _nextPosition = _player.position;
+        _nextPosition = _player.position + _player.velocity.normalized * 2f;
         _nextPosition += Vector3.up * Mathf.Cos(Mathf.Deg2Rad * _angle) * _distance;
         _nextPosition += Vector3.back * Mathf.Sin(Mathf.Deg2Rad * _angle) * _distance;
         transform.position = Vector3.Lerp(transform.position, _nextPosition, _speed * Time.deltaTime);
